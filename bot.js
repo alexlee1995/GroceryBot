b0f070361005c2388c861f4eeebecef61412eaae
Alexander Lee <alexlee1595@gmail.com>
2018-09-12 17:18:47 -0400

    Bot recognizes !ping followed by arguments

--- bot.js
+++ bot.js
@@ -18,22 +18,30 @@ bot.on('ready', function (evt) {
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
 bot.on('message', function (user, userID, channelID, message, evt) {
+    var items = [];
+    var dateIn = [];
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
     if (message.substring(0, 1) == '!') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
        
-        args = args.splice(1);
+        //args = args.splice(1);
         switch(cmd) {
             // !ping
             case 'ping':
+                var toSend = args[1];
                 bot.sendMessage({
                     to: channelID,
-                    message: 'Pong!'
+                    message: toSend
                 });
-            break;
-            // Just add any case commands if you want to..
+                break;
+            case 'test':
+                bot.sendMessage({
+                    to: channelID,
+                    message: "Fail1"
+                });
+                break;
          }
      }
 });
